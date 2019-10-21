411cb90e6d92d7f2ff59e2ca560bad49488bb501
Ryan Nemiroff <ryguyn@gmail.com>
2019-10-20 18:12:51 -0700

    Delete unused code

--- app/src/components/ExampleComponent.tsx
+++ app/src/components/ExampleComponent.tsx
@@ -1,17 +1,9 @@
-import React, { useState, useMemo } from "react";
+import React, { useState } from "react";
 import { Button, Input } from "reactstrap";
 import { useSelector, useDispatch } from "react-redux";
-// import { createSelector } from "reselect";
 
 import * as exampleActions from "redux/actions/exampleActions";
 import { AppState, selectors } from "redux/store";
-
-// const exampleMemoizedSelector = createSelector(
-//   [(state: AppState) => state.example.data],
-//   data => {
-//     return data + "!!!";
-//   }
-// );
 
 interface Props {
   exampleProp: string;
