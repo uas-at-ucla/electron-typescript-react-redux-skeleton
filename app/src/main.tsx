6ecabc84bb977784a185f0bae012c4beca2a4012
Ryan Nemiroff <ryguyn@gmail.com>
2021-09-11 17:59:34 -0700

    More errors shown as overlays

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { VechaiProvider } from "@vechaiui/react";
+import { ErrorPayload } from "vite/types/hmrPayload";
 
 import "./index.css";
 import App from "./components/App";
 import store from "./redux/store";
+import { relativePath } from "./utils/electronUtils";
 
 // declare Node.js properties of 'window' provided by Electron
 declare global {
@@ -20,6 +22,29 @@ if (process.env.NODE_ENV === "development") {
 } else {
   console.log("Running in production mode");
 }
+
+// Setup runtime error overlay (based on https://github.com/vitejs/vite/issues/2076#issuecomment-846558772)
+window.addEventListener("error", (errorEvent) => {
+  const ErrorOverlay = customElements.get("vite-error-overlay");
+  if (ErrorOverlay !== undefined) {
+    const err: ErrorPayload["err"] = {
+      message: errorEvent.message,
+      stack:
+        errorEvent.error instanceof Error &&
+        errorEvent.error.stack !== undefined
+          ? errorEvent.error.stack
+          : "",
+      loc: {
+        file:
+          relativePath(new URL(errorEvent.filename).pathname) ||
+          errorEvent.filename,
+        line: errorEvent.lineno,
+        column: errorEvent.colno,
+      },
+    };
+    document.body.appendChild(new ErrorOverlay(err));
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,13 +2,15 @@ import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import html from 'vite-plugin-html'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     reactRefresh(),
     tsconfigPaths(),
-    html()
+    html(),
+    eslintPlugin({'cache': false}) // TODO Cache feature needs to be fixed in vite-plugin-eslint (https://github.com/gxmari007/vite-plugin-eslint/issues/11)
   ],
   base: './' // Load static assets correctly in production
 })
